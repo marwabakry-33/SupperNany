363944d0e5436985ca1e70d26321e1df26af7868
marwabakry-33 <marwabakry284@gmail.com>
2025-04-19 22:44:57 +0200

    cd

--- project/settings.py
+++ project/settings.py
@@ -154,7 +154,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # تأكد من أن "static" موجود في نفس المجلد الرئيسي للمشروع
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
