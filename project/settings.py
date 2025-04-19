9ad0577070d037b1c5e215beb0b2a18b20d703a5
marwabakry-33 <marwabakry284@gmail.com>
2025-04-19 23:12:01 +0200

    cd

--- project/settings.py
+++ project/settings.py
@@ -157,6 +157,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),  # تأكد من أن "static" موجود في نفس المجلد الرئيسي للمشروع
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ده مكان تجميع الملفات بعد collectstatic
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
