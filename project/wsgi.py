98fa9cf0005f930b8f84a6e3162045ddbc5ccd61
marwabakry-33 <marwabakry284@gmail.com>
2025-04-20 03:02:15 +0200

    cd

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,18 +1,6 @@
 import os
-import sys
-
-# تفعيل البيئة الافتراضية
-activate_this = '/home/marwabakry23/SupperNany/.venv/bin/activate_this.py'
-exec(open(activate_this).read(), {'__file__': activate_this})
-
-# إضافة المسار إلى sys.path
-path = '/home/marwabakry23/SupperNany'
-if path not in sys.path:
-    sys.path.append(path)
-
-# تعيين إعدادات Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
-# استيراد تطبيق WSGI
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')  # غيّري 'project' لاسم مجلد الإعدادات عندك
+
 application = get_wsgi_application()
