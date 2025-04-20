aa7837aebe5bc554c57212c0a05bb694b844c390
marwabakry-33 <marwabakry284@gmail.com>
2025-04-20 02:19:16 +0200

    Fix WSGI configuration to activate virtual environment

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# تفعيل البيئة الافتراضية
+activate_this = '/home/marwabakry23/SupperNany/.venv/bin/activate_this.py'
+exec(open(activate_this).read(), {'__file__': activate_this})
 
+# إضافة المسار إلى sys.path
+path = '/home/marwabakry23/SupperNany'
+if path not in sys.path:
+    sys.path.append(path)
+
+# تعيين إعدادات Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
+# استيراد تطبيق WSGI
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
