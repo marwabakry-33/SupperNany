714fd70c248464ddc5e2de54c3151e329f632c92
marwabakry-33 <marwabakry284@gmail.com>
2025-05-13 03:00:44 +0300

    do quick tips

--- project/settings.py
+++ project/settings.py
@@ -33,9 +33,9 @@ EMAIL_HOST_PASSWORD = '5512m3rw3'  # ضع هنا كلمة مرور التطبي�
 DEFAULT_FROM_EMAIL = 'marwabakry284@gmail.com'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
-#ALLOWED_HOSTS = ['marwabakry23.pythonanywhere.com']
+DEBUG = False
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['marwabakry23.pythonanywhere.com']
 #'localhost', '127.0.0.1', '0.0.0.0', 
 
 # Application definition
