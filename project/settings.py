0af01aef66ca8422e4be5a9f1e40edd39781f850
marwabakry-33 <marwabakry284@gmail.com>
2025-07-29 01:28:13 +0300

    vaccination_dates

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 ALLOWED_HOSTS = ['marwabakry23.pythonanywhere.com','localhost', '127.0.0.1', '0.0.0.0']
 #'localhost', '127.0.0.1', '0.0.0.0', 
